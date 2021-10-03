99be41df086fa85af66d6d97b65df8a968eb0278
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2021-10-03 19:06:53 -0400

    feat: StorageRoomsService (updateCourseLastVisit)

--- src/services/StorageRoomsService.ts
+++ src/services/StorageRoomsService.ts
@@ -1,17 +1,19 @@
 import produce from "immer";
+import MoodleRoutesService from "./MoodleRoutesService";
 import { IRoom } from "../typings/IRoom";
 import StorageService from "./StorageService";
 import URLService from "./URLService";
+import { fallbackToLength } from "../utils/fallbackToLength";
 
 class StorageRoomsService {
   getAll = () => StorageService.getItem("rooms", [] as IRoom[]);
 
   getOneByUrl = async (
-    url: string,
+    roomUrl: string,
     incomingRooms: IRoom[] | Promise<IRoom[]> = this.getAll()
   ) => {
     const rooms = await Promise.resolve(incomingRooms);
-    const normalizedUrl = URLService.normalize(url);
+    const normalizedUrl = URLService.normalize(roomUrl);
     return (
       rooms.find(({ url }) =>
         normalizedUrl.startsWith(URLService.normalize(url))
@@ -21,21 +23,6 @@ class StorageRoomsService {
 
   isRoomInStorage = async (url: string) =>
     (await this.getOneByUrl(url)) !== null;
-
-  updateOne = async (courseUrl: string, callback: (room: IRoom) => void) => {
-    await this.updateMany(async (rooms) => {
-      const roomIdx = await this.getMatchedIndex(courseUrl, rooms);
-      callback(rooms[roomIdx]);
-    });
-  };
-
-  updateMany = async (callback: (rooms: IRoom[]) => void) => {
-    const rooms = await this.getAll();
-
-    const updatedRooms = await Promise.resolve(produce(rooms, callback));
-
-    await StorageService.setData({ rooms: updatedRooms });
-  };
 
   getMatchedIndex = async (
     roomUrl: string,
@@ -49,6 +36,47 @@ class StorageRoomsService {
         normalizedRoomUrl.startsWith(URLService.normalize(loopRoomUrl))
       ) ?? -1
     );
+  };
+
+  updateOne = async (roomUrl: string, callback: (room: IRoom) => void) => {
+    await this.updateMany(async (rooms) => {
+      const roomIdx = await this.getMatchedIndex(roomUrl, rooms);
+      callback(rooms[roomIdx]);
+    });
+  };
+
+  updateMany = async (callback: (rooms: IRoom[]) => void) => {
+    const rooms = await this.getAll();
+
+    const updatedRooms = await Promise.resolve(produce(rooms, callback));
+
+    await StorageService.setData({ rooms: updatedRooms });
+  };
+
+  updateCourseLastVisit = async (
+    courseUrl: string,
+    lastVisitAt = Date.now()
+  ) => {
+    const courseId = MoodleRoutesService.getCourseId(courseUrl);
+
+    if (!courseId) return;
+
+    await this.updateOne(courseUrl, (room) => {
+      const idx = fallbackToLength(
+        room.coursesCache.findIndex((i) => i.url === courseUrl),
+        room.coursesCache.length
+      );
+
+      const cache = {
+        ...{ courseId: courseId, url: courseUrl, name: courseUrl },
+        ...room.coursesCache[idx],
+      };
+
+      room.coursesCache[idx] = {
+        ...cache,
+        lastVisitAt,
+      };
+    });
   };
 }
 
---
+++ src/utils/fallbackToLength.ts
@@ -0,0 +1,2 @@
+export const fallbackToLength = (idx: number, length: number) =>
+  idx === -1 ? length : idx;
