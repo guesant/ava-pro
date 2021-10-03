081bc176cc3949175f80e9fb3a406a5faa9d680f
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2021-10-03 19:12:58 -0400

    feat: ILocalStore (rooms); IRoom

--- src/typings/ILocalStore.ts
+++ src/typings/ILocalStore.ts
@@ -1,5 +1,7 @@
+import { IRoom } from "./IRoom";
 import { ISettings } from "./ISettings";
 
 export type ILocalStore = {
+  rooms: IRoom[];
   settings: ISettings;
 };
---
+++ src/typings/IRoom.ts
@@ -0,0 +1,15 @@
+export type IRoomCourseMeta = {};
+
+export type IRoomCourse<T = IRoomCourseMeta> = {
+  meta?: T;
+  url: string;
+  name: string;
+  courseId: string;
+  lastVisitAt: number;
+};
+
+export type IRoom = {
+  url: string;
+  name: string;
+  coursesCache: IRoomCourse[];
+};
