896161c579b08cfe516f8bc8e4aab0097d106301
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2021-10-03 22:02:37 -0400

    feat: StorageRoomsService (add, removeOne, fetchCourses)

--- src/services/StorageRoomsService.ts
+++ src/services/StorageRoomsService.ts
@@ -1,11 +1,16 @@
 import produce from "immer";
+import { IRoom, IRoomCourse } from "../typings/IRoom";
+import { fallbackToLength } from "../utils/fallbackToLength";
 import MoodleRoutesService from "./MoodleRoutesService";
-import { IRoom } from "../typings/IRoom";
+import MoodleService from "./MoodleService";
 import StorageService from "./StorageService";
 import URLService from "./URLService";
-import { fallbackToLength } from "../utils/fallbackToLength";
 
+type IHandleFetchRoomCoursesOptions = { keepUnknownCourses?: boolean };
 class StorageRoomsService {
+  isRoomInStorage = async (url: string) =>
+    (await this.getOneByUrl(url)) !== null;
+
   getAll = () => StorageService.getItem("rooms", [] as IRoom[]);
 
   getOneByUrl = async (
@@ -21,9 +26,6 @@ class StorageRoomsService {
     );
   };
 
-  isRoomInStorage = async (url: string) =>
-    (await this.getOneByUrl(url)) !== null;
-
   getMatchedIndex = async (
     roomUrl: string,
     incomingRooms: IRoom[] | Promise<IRoom[]> = this.getAll()
@@ -38,6 +40,15 @@ class StorageRoomsService {
     );
   };
 
+  add = async (data: Omit<IRoom, "coursesCache">) => {
+    const room = { coursesCache: [], ...data };
+    await this.updateMany((rooms) => ([] as IRoom[]).concat(rooms, room));
+    return room;
+  };
+
+  removeOne = (url: string) =>
+    this.updateMany((rooms) => rooms.filter((i) => i.url !== url));
+
   updateOne = async (roomUrl: string, callback: (room: IRoom) => void) => {
     await this.updateMany(async (rooms) => {
       const roomIdx = await this.getMatchedIndex(roomUrl, rooms);
@@ -45,10 +56,14 @@ class StorageRoomsService {
     });
   };
 
-  updateMany = async (callback: (rooms: IRoom[]) => void) => {
+  updateMany = async (
+    callback: (
+      rooms: IRoom[]
+    ) => void | IRoom[] | Promise<IRoom[]> | Promise<void>
+  ) => {
     const rooms = await this.getAll();
 
-    const updatedRooms = await Promise.resolve(produce(rooms, callback));
+    const updatedRooms = await Promise.resolve(produce(rooms, callback as any));
 
     await StorageService.setData({ rooms: updatedRooms });
   };
@@ -76,6 +91,35 @@ class StorageRoomsService {
         ...cache,
         lastVisitAt,
       };
+    });
+  };
+
+  fetchCourses = async (
+    url: string,
+    { keepUnknownCourses = true }: IHandleFetchRoomCoursesOptions = {}
+  ) => {
+    const courses = await MoodleService.fetchRoomCourses(url);
+
+    await this.updateOne(url, (draft) => {
+      if (!keepUnknownCourses) {
+        draft.coursesCache = draft.coursesCache.filter(
+          (course) =>
+            courses.findIndex((i) => i.courseId === course.courseId) !== -1
+        );
+      }
+      for (const course of courses) {
+        const courseIndex = fallbackToLength(
+          draft.coursesCache.findIndex((i) => i.courseId === course.courseId),
+          draft.coursesCache.length
+        );
+
+        draft.coursesCache[courseIndex] = {
+          ...(draft.coursesCache[courseIndex] || {
+            lastVisitAt: Date.now(),
+          }),
+          ...course,
+        } as IRoomCourse;
+      }
     });
   };
 }
