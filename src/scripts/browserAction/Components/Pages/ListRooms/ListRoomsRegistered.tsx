30078ba152cc243b909f7a295fec1c5cbc45e005
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2021-11-01 20:17:04 -0400

    refactor: browserAction: ListRooms: ListRoomsRegisteredItem

--- src/scripts/browserAction/Components/Pages/ListRooms/ListRoomsRegistered.tsx
+++ src/scripts/browserAction/Components/Pages/ListRooms/ListRoomsRegistered.tsx
@@ -1,45 +1,8 @@
-import { IRoom } from "../../../../../typings/IRoom";
-import { IHandleOpenMenuClick, useRoomOptionsMenu } from "./useRoomOptionsMenu";
-import { useHandleRoomClick } from "./useHandleRoomClick";
-import { Fragment, useCallback } from "react";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
-import Typography from "@mui/material/Typography";
-import ListItemSecondaryAction from "@mui/material/ListItemSecondaryAction";
-import IconButton from "@mui/material/IconButton";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
-import Divider from "@mui/material/Divider";
+import { useRoomOptionsMenu } from "./useRoomOptionsMenu";
 import { useContextSelector } from "use-context-selector";
 import { RoomsContext } from "../../../Hooks/RoomsContext";
 import List from "@mui/material/List";
-
-const ListRoomsItem: React.FC<{
-  room: IRoom;
-  handleOpenMenuClick: IHandleOpenMenuClick;
-}> = ({ handleOpenMenuClick, room: { name, url } }) => {
-  const handleRoomClick = useHandleRoomClick();
-
-  const handleRoomListItemClick = useCallback(
-    () => handleRoomClick(url),
-    [url, handleRoomClick]
-  );
-
-  return (
-    <Fragment key={url}>
-      <ListItem button onClick={handleRoomListItemClick}>
-        <ListItemText>
-          <Typography noWrap>{name}</Typography>
-        </ListItemText>
-        <ListItemSecondaryAction onClick={(e) => e.stopPropagation()}>
-          <IconButton color="inherit" onClick={handleOpenMenuClick(url)}>
-            <MoreVertIcon />
-          </IconButton>
-        </ListItemSecondaryAction>
-      </ListItem>
-      <Divider />
-    </Fragment>
-  );
-};
+import ListRoomsRegisteredItem from "./ListRoomsRegisteredItem";
 
 const ListRoomsRegistered = () => {
   const { menu, handleOpenMenuClick } = useRoomOptionsMenu();
@@ -52,7 +15,7 @@ const ListRoomsRegistered = () => {
   return (
     <List>
       {rooms.map((room) => (
-        <ListRoomsItem
+        <ListRoomsRegisteredItem
           room={room}
           key={room.url}
           handleOpenMenuClick={handleOpenMenuClick}
---
+++ src/scripts/browserAction/Components/Pages/ListRooms/ListRoomsRegisteredItem.tsx
@@ -0,0 +1,46 @@
+import React, { Fragment, useCallback } from "react";
+import { IRoom } from "../../../../../typings/IRoom";
+import { IHandleOpenMenuClick } from "./useRoomOptionsMenu";
+import { useHandleRoomClick } from "./useHandleRoomClick";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Typography from "@mui/material/Typography";
+import ListItemSecondaryAction from "@mui/material/ListItemSecondaryAction";
+import IconButton from "@mui/material/IconButton";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import Divider from "@mui/material/Divider";
+
+type IListRoomsRegisteredItemProps = {
+  room: IRoom;
+  handleOpenMenuClick: IHandleOpenMenuClick;
+};
+
+const ListRoomsRegisteredItem: React.FC<IListRoomsRegisteredItemProps> = ({
+  handleOpenMenuClick,
+  room: { name, url },
+}) => {
+  const handleRoomClick = useHandleRoomClick();
+
+  const handleRoomListItemClick = useCallback(
+    () => handleRoomClick(url),
+    [url, handleRoomClick]
+  );
+
+  return (
+    <Fragment key={url}>
+      <ListItem button onClick={handleRoomListItemClick}>
+        <ListItemText>
+          <Typography noWrap>{name}</Typography>
+        </ListItemText>
+        <ListItemSecondaryAction onClick={(e) => e.stopPropagation()}>
+          <IconButton color="inherit" onClick={handleOpenMenuClick(url)}>
+            <MoreVertIcon />
+          </IconButton>
+        </ListItemSecondaryAction>
+      </ListItem>
+      <Divider />
+    </Fragment>
+  );
+};
+
+export default ListRoomsRegisteredItem;
