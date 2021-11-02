0c8af1213118bc7f3445f37a6494f41065772289
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2021-11-01 21:03:26 -0400

    style: browserAction: useStorage: use MayBePromise in extractData

--- src/scripts/browserAction/Hooks/useStorage.ts
+++ src/scripts/browserAction/Hooks/useStorage.ts
@@ -1,9 +1,10 @@
 import { useEffect } from "react";
 import { useQuery } from "react-query";
+import { MayBePromise } from "../../../typings/MayBePromise";
 
 export const useStorage = <T>(
   keys: string[],
-  extractData: () => T | Promise<T>,
+  extractData: () => MayBePromise<T>,
   defaultValue: T
 ) => {
   const { data, refetch, ...rest } = useQuery(keys, extractData);
