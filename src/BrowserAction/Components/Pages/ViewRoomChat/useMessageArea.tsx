daeed50c3260d15708af137827b3b85893c2edc0
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2021-11-03 09:43:10 -0400

    feat: BrowserAction: ViewRoomChat: useMessageArea: marca todas as mensagens como lidas

--- src/BrowserAction/Components/Pages/ViewRoomChat/useMessageArea.tsx
+++ src/BrowserAction/Components/Pages/ViewRoomChat/useMessageArea.tsx
@@ -6,17 +6,19 @@ import {
   appendInfiniteQueryData,
   InfiniteData,
 } from "../../../../helpers/appendInfiniteQueryData";
-import { IMessageArea } from "../../../../typings/MoodleAPI/IMessageArea";
-import { ViewRoomAuthedContext } from "../ViewRoom/ViewRoomAuthedContext";
 import { CoreMessage_DataForMessageArea_Messages } from "../../../../services/MoodleAPI/CoreMessage/DataForMessageArea/Messages";
+import { CoreMessage_MarkAllMessagesAsRead } from "../../../../services/MoodleAPI/CoreMessage/MarkAllMessagesAsRead";
 import { fetchUserId } from "../../../../services/MoodleAPI/fetchUserId";
+import { IMessageArea } from "../../../../typings/MoodleAPI/IMessageArea";
 import { IMessageAreaMessage } from "../../../../typings/MoodleAPI/IMessageAreaMessage";
+import { ViewRoomAuthedContext } from "../ViewRoom/ViewRoomAuthedContext";
 
-const messagesPerQuery = 10;
+const MESSAGES_PER_QUERY = 10;
+const IS_AUTO_FETCH_UPDATES_ENABLED = true;
 
-export const useMessageArea = (contactId: number) => {
+export const useMessageArea = (contactid: number) => {
   const queryClient = useQueryClient();
-  const queryKey = ["messages", contactId];
+  const queryKey = ["messages", contactid];
 
   const [isFetchingUpdates, setIsFetchingUpdates] = useState(false);
 
@@ -30,23 +32,36 @@ export const useMessageArea = (contactId: number) => {
   );
 
   const fetchMessageArea = useCallback(
-    async (extraArgs: Record<string, any> = {}) => {
+    async (args: Record<string, any> = {}) => {
       if (authedHttp) {
         const userid = (await fetchUserId(authedHttp)())!;
-        return CoreMessage_DataForMessageArea_Messages(authedHttp)({
-          currentuserid: userid,
-          otheruserid: contactId,
-          ...extraArgs,
-        });
+
+        const response = await CoreMessage_DataForMessageArea_Messages(
+          authedHttp
+        )({ currentuserid: userid, otheruserid: contactid, ...args });
+
+        if (response && response.messages.length > 0) {
+          if (
+            (args.limitfrom === undefined || args.limitfrom === 0) &&
+            (args.newest === undefined || args.newest === true)
+          ) {
+            await CoreMessage_MarkAllMessagesAsRead(authedHttp)({
+              useridfrom: contactid,
+              useridto: userid,
+            });
+          }
+        }
+
+        return response;
       }
       return null;
     },
-    [authedHttp, contactId]
+    [authedHttp, contactid]
   );
 
   const fetchMessageAreaAndSync = useCallback(
-    async (extraArgs: Record<string, any> = {}) =>
-      fetchMessageArea(extraArgs).then((response) => {
+    async (args: Record<string, any> = {}) =>
+      fetchMessageArea(args).then((response) => {
         setLastMessageArea(response);
         return response;
       }),
@@ -56,43 +71,35 @@ export const useMessageArea = (contactId: number) => {
   const fetchMessages = ({ pageParam = 0 }) =>
     fetchMessageAreaAndSync({
       limitfrom: pageParam,
-      limitnum: messagesPerQuery,
+      limitnum: MESSAGES_PER_QUERY,
     })
       .then((response) => response?.messages ?? [])
       .then((messages) => Array.from(messages).reverse())
       .then((messages) => ({
         data: messages,
         nextCursor:
-          messages.length === messagesPerQuery
-            ? pageParam + messagesPerQuery
+          messages.length === MESSAGES_PER_QUERY
+            ? pageParam + MESSAGES_PER_QUERY
             : undefined,
       }));
 
-  const infiniteQuery = useInfiniteQuery(
-    ["messages", contactId],
-    fetchMessages,
-    {
-      getNextPageParam: ({ nextCursor }) => nextCursor,
-    }
-  );
+  const infiniteQuery = useInfiniteQuery(queryKey, fetchMessages, {
+    getNextPageParam: ({ nextCursor }) => nextCursor,
+  });
 
   const lastMessageTimeSent = useMemo(() => {
     const pages = infiniteQuery.data?.pages || [];
-
     const messageAreas = pages.map((i) => i.data).flat(2);
-
-    if (messageAreas.length === 0) {
-      return NaN;
-    }
-
-    return messageAreas.reduce((acc, i) => Math.max(acc, i.timecreated), 0);
+    return messageAreas.length === 0
+      ? NaN
+      : messageAreas.reduce((acc, i) => Math.max(acc, i.timecreated), 0);
   }, [infiniteQuery.data]);
 
   const fetchUpdates = useCallback(async () => {
     if (lastMessageTimeSent !== 0 && !isFetchingUpdates) {
       setIsFetchingUpdates(true);
 
-      const response = await fetchMessageArea({
+      const response = await fetchMessageAreaAndSync({
         timefrom: lastMessageTimeSent + 1,
       });
 
@@ -111,9 +118,8 @@ export const useMessageArea = (contactId: number) => {
             const updatedData = appendInfiniteQueryData(
               previousData,
               messages,
-              messagesPerQuery
+              MESSAGES_PER_QUERY
             );
-
             queryClient.setQueryData(queryKey, updatedData);
           }
         }
@@ -121,16 +127,19 @@ export const useMessageArea = (contactId: number) => {
 
       setIsFetchingUpdates(false);
     }
-  }, [queryClient, isFetchingUpdates, lastMessageTimeSent]);
+  }, [
+    queryClient,
+    isFetchingUpdates,
+    lastMessageTimeSent,
+    fetchMessageAreaAndSync,
+  ]);
 
   const _fetchUpdates = useCallback(throttle(fetchUpdates, 2 * 1000), [
     fetchUpdates,
   ]);
 
-  const isAutoFetchUpdatesEnabled = true;
-
   useEffect(() => {
-    if (isAutoFetchUpdatesEnabled) {
+    if (IS_AUTO_FETCH_UPDATES_ENABLED) {
       const intervalId = setInterval(() => {
         _fetchUpdates();
       }, 60 * 1000);
