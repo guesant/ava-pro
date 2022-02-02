3555e00a305089e8e1becfc8d3cf2b81bb97677e
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2022-02-01 22:30:04 -0400

    fix(moodle-client-js): features: auth: check-login: extract-login-state-from-page: improve the check login algorithm

--- packages/moodle-client-js/lib/features/auth/check-login/extract-login-state-from-page.ts
+++ packages/moodle-client-js/lib/features/auth/check-login/extract-login-state-from-page.ts
@@ -1,5 +1,7 @@
 export const extractLoginStateFromPage = (data: string) => {
-  const [body = ""] = data.match(/<body[^>]*>/g) || []
-  const isLoggedIn = !body.includes("notloggedin")
-  return isLoggedIn
+  // const [body = ""] = data.match(/<body[^>]*>/g) || []
+  // return !body.includes("notloggedin")
+
+  // eslint-disable-next-line quotes
+  return !data.includes('id="login"')
 }
